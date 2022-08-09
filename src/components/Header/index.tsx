0775879249e55ed22ff2c78b8eb53a66d4f2f011
Carlos Henrique <carloshenriquepvh@hotmail.com>
2022-08-09 10:43:10 -0400

    Construindo um Hook de carrinho de compras

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
 
 import logo from '../../assets/images/logo.svg';
 import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
+import { api } from '../../services/api';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+  const [cartSize, setCartSize] = useState(0);
+
+  // useEffect(() => {
+  //   console.log('estou no useeffect1');
+  //   api.get('products').then( response => {
+  //     console.log(response.data);
+  //   })
+
+  // },[]);
 
   return (
     <Container>
@@ -20,7 +29,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
